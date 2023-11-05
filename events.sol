4cace906e68a09d5dedf1bd5ac0509918193e772
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-05 20:11:06 +0530

    emit

--- events.sol
+++ events.sol
@@ -1,1 +1,10 @@
 //SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+contract events {
+    event mess (address indexed sender, string message);
+    event example();
+    function messages () public {
+        emit mess (msg.sender, "hello, world");
+        emit example();
+    } 
+}
